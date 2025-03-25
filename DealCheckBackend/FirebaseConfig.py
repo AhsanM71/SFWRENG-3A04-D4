25b04eaf4250b817aa32c6ac6418aca226b6cdbe
Abdallah Alqashqish <abdalq2003@gmail.com>
2025-03-25 13:13:38 -0400

    Setup firebase auth and firestore instances

--- DealCheckBackend/FirebaseConfig.py
+++ DealCheckBackend/FirebaseConfig.py
@@ -1,6 +1,9 @@
 import firebase_admin
-from firebase_admin import credentials, auth
+from firebase_admin import credentials, auth as authentication, firestore
+import firebase_admin.firestore
 
 # Initialize the firebase app with the project credentials.
 cred = credentials.Certificate("./serviceAccountKey.json")
-firebase_admin.initialize_app(credential=cred)
+app = firebase_admin.initialize_app(credential=cred)
+auth = authentication.Client(app=app)
+db = firestore.client()
