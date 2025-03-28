afbd9a377f3ca7b234eaefe94fba70b990b652ba
Abdallah Alqashqish <abdalq2003@gmail.com>
2025-03-28 08:09:25 -0400

    Implement FirebaseDatabaseEntity interface with Car data class

--- DealCheckBackend/core/data/Car.py
+++ DealCheckBackend/core/data/Car.py
@@ -1,10 +1,31 @@
-class Car:
+from __future__ import annotations
+from data import FirestoreDatabaseEntity
+
+class Car(FirestoreDatabaseEntity):
+    @staticmethod
+    def from_dict(data: dict) -> Car:
+        return Car(
+            id=data['id'],
+            make=data['make'],
+            model=data['model'],
+            year=data['year'],
+            mileage=data['mileage']
+        )
+
     def __init__(self, id: int, make: str, model: str, year: int, mileage: float):
         self.id = id
         self.make = make
         self.model = model
         self.year = year
         self.mileage = mileage
+
+    def to_dict(self):
+        return {
+            'make': self.getMake(),
+            'model': self.getModel(),
+            'year': self.getYear(),
+            'mileage': self.getMileage()
+        }
 
     def getId(self) -> int:
         return self.id
