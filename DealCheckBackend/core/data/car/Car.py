156937afd0c45dd9bb16932cdc7ac518e8c572e0
Abdallah Alqashqish <abdalq2003@gmail.com>
2025-03-29 18:23:02 -0400

    Add remaining car fields

--- DealCheckBackend/core/data/car/Car.py
+++ DealCheckBackend/core/data/car/Car.py
@@ -9,22 +9,53 @@ class Car(FirestoreDatabaseEntity):
             make=data['make'],
             model=data['model'],
             year=data['year'],
-            mileage=data['mileage']
+            trim=data['trim'],
+            mileage=data['mileage'],
+            condition=data['condition'],
+            accident_history=data['accident_history'],
+            previous_owners=data['previous_owners'],
+            image=data['img'],
+            description=data['descritpion']
         )
 
-    def __init__(self, id: int, make: str, model: str, year: int, mileage: float):
+    def __init__(
+            self, 
+            id: int, 
+            make: str, 
+            model: str, 
+            year: int,
+            trim: str,
+            mileage: float,
+            condition: str,
+            accident_history: bool,
+            previous_owners: int,
+            image: str,
+            description: str
+        ):
         self.id = id
         self.make = make
         self.model = model
         self.year = year
+        self.trim = trim
         self.mileage = mileage
+        self.condition = condition
+        self.accident_history = accident_history
+        self.previous_owners = previous_owners
+        self.image = image
+        self.description = description
 
     def to_dict(self):
         return {
             'make': self.getMake(),
             'model': self.getModel(),
             'year': self.getYear(),
-            'mileage': self.getMileage()
+            'trim': self.getTrim(),
+            'mileage': self.getMileage(),
+            'condition': self.getCondition(),
+            'accident_history': self.getAccidentHistory(),
+            'previous_owners': self.getPreviousOwners(),
+            'image': self.getImageSource(),
+            'description': self.getDescription()
         }
 
     def getId(self) -> int:
@@ -39,5 +70,23 @@ class Car(FirestoreDatabaseEntity):
     def getYear(self) -> int:
         return self.year
     
+    def getTrim(self) -> int:
+        return self.trim
+    
     def getMileage(self) -> float:
         return self.mileage
+    
+    def getCondition(self) -> str:
+        return self.condition
+    
+    def getAccidentHistory(self) -> bool:
+        return self.accident_history
+    
+    def getPreviousOwners(self) -> int:
+        return self.previous_owners
+    
+    def getImageSource(self) -> str:
+        return self.image
+    
+    def getDescription(self) -> str:
+        return self.description
